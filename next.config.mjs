d969fa6cd23c33d4cbed6db5745fe091643c7847
JUSTICEESSIELP <justicessiel@gmail.com>
2025-06-20 18:55:19 +0000

    (fix): next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,39 @@
 const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
-      },
-      images: {
+    },
+    images: {
         domains: ["res.cloudinary.com"],
-      },
-      webpack: (config) => {
+    },
+    webpack: (config) => {
         config.externals.push("pino-pretty", "lokijs", "encoding");
         return config;
-      },
+    },
+    async headers() {
+        return [
+            {
+                source: '/.well-known/matrix/:path*',
+                headers: [
+                    {
+                        key: 'Content-Type',
+                        value: 'application/json',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET, OPTIONS',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'Content-Type',
+                    },
+                ],
+            },
+        ]
+    },
 };
 
 export default nextConfig;
